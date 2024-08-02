deb3c0a62f62e4643b04768e825fbbb0df935a13
shrikantvsnv <shrikantvsnv@gmail.com>
2024-08-02 09:30:39 +0530

    jsonresponse

---
+++ JsonResponseTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Traits;
+
+trait JSONResponseTrait
+{
+    /**
+     * Return a JSON success response.
+     *
+     * @param mixed $data
+     * @param string $message
+     * @param int $statusCode
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function successResponse($data = null, $message = 'Success', $statusCode = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+        ], $statusCode);
+    }
+
+    /**
+     * Return a JSON error response.
+     *
+     * @param string $message
+     * @param int $statusCode
+     * @param mixed $errors
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function errorResponse($message = 'Error', $statusCode = 400, $errors = null)
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'errors' => $errors,
+        ], $statusCode);
+    }
+}
