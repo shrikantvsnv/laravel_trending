096798618ff6c87e6c54559b0d1c380ffdd15041
shrikantvsnv <shrikantvsnv@gmail.com>
2024-08-02 09:37:59 +0530

    jsonresponse

--- JsonResponseTrait.php
+++ JsonResponseTrait.php
@@ -12,7 +12,7 @@ trait JSONResponseTrait
      * @param int $statusCode
      * @return \Illuminate\Http\JsonResponse
      */
-    public function successResponse($data = null, $message = 'Success', $statusCode = 200)
+    public function successResponse($data = null, $message = 'Success', $statusCode = 200) : JsonResponse
     {
         return response()->json([
             'status' => 'success',
@@ -29,7 +29,7 @@ trait JSONResponseTrait
      * @param mixed $errors
      * @return \Illuminate\Http\JsonResponse
      */
-    public function errorResponse($message = 'Error', $statusCode = 400, $errors = null)
+    public function errorResponse($message = 'Error', $statusCode = 400, $errors = null) :JsonResponse
     {
         return response()->json([
             'status' => 'error',
