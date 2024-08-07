a7a048e314b1aafe6574fedca7e08af1c30d3a1b
shrikantvsnv <shrikantvsnv@gmail.com>
2024-08-07 08:50:31 +0530

    base model

--- BaseModel.php
+++ BaseModel.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseModel extends Model
+{
+    protected $connection = 'default_connection'; // Default connection, can be overridden
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        // You can dynamically set the connection based on certain conditions
+        // $this->setConnection('mysql'); // Uncomment and modify as needed
+    }
+}
+
+namespace App\Models;
+
 class User extends BaseModel
 {
-    protected $connection = 'mysql'; // Specify the connection for this model
+    protected $connection = 'mysql'; // Specify the connection for this model if required otherwise base class connection would apply
 
     protected $fillable = [
         'name', 'email', 'password',
@@ -23,7 +40,7 @@ namespace App\Models;
 
 class Order extends BaseModel
 {
-    protected $connection = 'mysql2'; // Specify the connection for this model
+    protected $connection = 'mysql2'; // Specify the connection for this model if required otherwise base class connection would apply
 
     protected $fillable = [
         'order_number', 'user_id', 'amount',
